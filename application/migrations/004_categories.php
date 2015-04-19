5feadc7ef47345511df606c1b34fb94f4c549404
khadim-raath <khadim.raath@incubasys>
2015-04-19 12:47:03 +0500

    db migrations added

--- application/migrations/004_categories.php
+++ application/migrations/004_categories.php
@@ -2,23 +2,16 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Migration_advertisements extends CI_Migration {
+class Migration_categories extends CI_Migration {
 
     public function up() {
-        $table = "advertisements";
+        $table = "categories";
 
         $this->dbforge->add_field("id int(11) unsigned NOT NULL AUTO_INCREMENT");
         $this->dbforge->add_field("title varchar(255) NOT NULL");
-        $this->dbforge->add_field("price int(11) NOT NULL DEFAULT 0");
-        $this->dbforge->add_field("location varchar(255)");
-        $this->dbforge->add_field("category varchar(255)");
-        $this->dbforge->add_field("image_url varchar(255)");
-       
-        $this->dbforge->add_field("images varchar(255)");
-        $this->dbforge->add_field("stock int(11)");
         
          $this->dbforge->add_field("status BOOLEAN NOT NULL DEFAULT 0 ");
-         $this->dbforge->add_field("description varchar(255)");
+         
         $this->dbforge->add_field("created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP");
 
         $this->dbforge->add_key('id', TRUE);
@@ -28,7 +21,7 @@ class Migration_advertisements extends CI_Migration {
     }
 
     public function down() {
-        $table = "advertisements";
+        $table = "categories";
         $this->dbforge->drop_table($table);
     }
 
